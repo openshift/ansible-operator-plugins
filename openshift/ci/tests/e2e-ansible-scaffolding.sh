4fb1f647bba1b55556ff0c28c1dd4297cdf58a3d
chiragkyal <ckyal@redhat.com>
2025-01-31 16:46:49 +0530

    UPSTREAM: <carry>: Updates downstream e2e test to use upstream `testdata/memcached-molecule-operator`
    - Adds RBAC permission for `project.openshift.io`.
    - Adds `--ignore-not-found` with `make undeploy` to avoid errors
    when some kustomized resources are deleted with namespace deletion.
    
    Signed-off-by: chiragkyal <ckyal@redhat.com>

--- openshift/ci/tests/e2e-ansible-scaffolding.sh
+++ openshift/ci/tests/e2e-ansible-scaffolding.sh
@@ -4,7 +4,6 @@ source hack/lib/common.sh
 
 set -eux
 
-component="osdk-ansible-e2e"
 eval IMAGE=$IMAGE_FORMAT
 ROOTDIR="$(pwd)"
 GOTMP="$(mktemp -d -p $GOPATH/src)"
@@ -37,26 +36,26 @@ test_operator() {
     echo "Entered test_operator"
 
     # wait for operator pod to run
-    if ! timeout 1m kubectl rollout status deployment/memcached-operator-controller-manager -n memcached-operator-system;
+    if ! timeout 1m kubectl rollout status deployment/memcached-molecule-operator-controller-manager -n memcached-molecule-operator-system;
     then
         echo FAIL: operator failed to run
         kubectl describe pods
-        kubectl logs deployment/memcached-operator-controller-manager -c manager
+        kubectl logs deployment/memcached-molecule-operator-controller-manager -c manager
         exit 1
     fi
 
 
     # verify that metrics service was created
-    if ! timeout 60s bash -c -- "until kubectl get service/memcached-operator-controller-manager-metrics-service > /dev/null 2>&1; do sleep 1; done";
+    if ! timeout 60s bash -c -- "until kubectl get service/memcached-molecule-operator-controller-manager-metrics-service > /dev/null 2>&1; do sleep 1; done";
     then
         echo "Failed to get metrics service"
         kubectl describe pods
-        kubectl logs deployment/memcached-operator-controller-manager -c manager
+        kubectl logs deployment/memcached-molecule-operator-controller-manager -c manager
         exit 1
     fi
 
     # create the service-account-token for the default service account
-    cat <<EOF | kubectl apply -n memcached-operator-system -f -
+    cat <<EOF | kubectl apply -n memcached-molecule-operator-system -f -
 apiVersion: v1
 kind: Secret
 type: kubernetes.io/service-account-token
@@ -71,11 +70,11 @@ EOF
 
 
     # verify that the metrics endpoint exists
-    if ! timeout 1m bash -c -- "until kubectl run --attach --rm --restart=Never test-metrics --image=registry.access.redhat.com/ubi8/ubi-minimal:latest -n memcached-operator-system --overrides='{\"spec\":{\"securityContext\":{\"runAsNonRoot\": true, \"capabilities\": {\"drop\": [\"ALL\"]}, \"allowPrivelegeEscalation\": false, \"seccompProfile\": {\"type\": \"RuntimeDefault\"}}}}' -- curl -sfkH \"Authorization: Bearer ${token}\" https://memcached-operator-controller-manager-metrics-service:8443/metrics; do sleep 1; done";
+    if ! timeout 1m bash -c -- "until kubectl run --attach --rm --restart=Never test-metrics --image=registry.access.redhat.com/ubi8/ubi-minimal:latest -n memcached-molecule-operator-system --overrides='{\"spec\":{\"securityContext\":{\"runAsNonRoot\": true, \"capabilities\": {\"drop\": [\"ALL\"]}, \"allowPrivelegeEscalation\": false, \"seccompProfile\": {\"type\": \"RuntimeDefault\"}}}}' -- curl -sfkH \"Authorization: Bearer ${token}\" https://memcached-molecule-operator-controller-manager-metrics-service:8443/metrics; do sleep 1; done";
     then
         echo "Failed to verify that metrics endpoint exists"
         kubectl describe pods
-        kubectl logs deployment/memcached-operator-controller-manager -c manager
+        kubectl logs deployment/memcached-molecule-operator-controller-manager -c manager
         exit 1
     fi
 
@@ -87,7 +86,7 @@ EOF
     then
         echo FAIL: operator failed to create memcached Deployment
         kubectl describe pods
-        kubectl logs deployment/memcached-operator-controller-manager -c manager
+        kubectl logs deployment/memcached-molecule-operator-controller-manager -c manager
         exit 1
     fi
 
@@ -96,7 +95,7 @@ EOF
     then
         echo FAIL: memcached Deployment failed rollout
         kubectl describe pods
-        kubectl logs deployment/memcached-operator-controller-manager -c manager
+        kubectl logs deployment/memcached-molecule-operator-controller-manager -c manager
         kubectl logs deployment/memcached-sample-memcached
         exit 1
     fi
@@ -115,7 +114,7 @@ EOF
     # then
     #     echo FAIL: the finalizer did not delete the configmap
     #     kubectl describe pods
-    #     kubectl logs deployment/memcached-operator-controller-manager -c manager
+    #     kubectl logs deployment/memcached-molecule-operator-controller-manager -c manager
     #     exit 1
     # fi
 
@@ -124,52 +123,70 @@ EOF
     then
         echo FAIL: memcached Deployment did not get garbage collected
         kubectl describe pods
-        kubectl logs deployment/memcached-operator-controller-manager -c manager
+        kubectl logs deployment/memcached-molecule-operator-controller-manager -c manager
         exit 1
     fi
 
     # Ensure that no errors appear in the log
-    if kubectl logs deployment/memcached-operator-controller-manager -c manager | grep -i error;
+    if kubectl logs deployment/memcached-molecule-operator-controller-manager -c manager | grep -i error;
     then
         echo FAIL: the operator log includes errors
         kubectl describe pods
-        kubectl logs deployment/memcached-operator-controller-manager -c manager
+        kubectl logs deployment/memcached-molecule-operator-controller-manager -c manager
         exit 1
     fi
 }
 
 # use sample in testdata
-pushd $ROOTDIR/openshift/ci/testdata/ansible/memcached-operator
+pushd $ROOTDIR/testdata/memcached-molecule-operator
 ls
 
 # deploy operator
 echo "running make deploy"
 make deploy IMG=$IMAGE
 
-# create clusterrolebinding for metrics
-kubectl create clusterrolebinding memcached-operator-metrics-reader-rolebinding --clusterrole=memcached-operator-metrics-reader --serviceaccount=memcached-operator-system:default
+# Hack: Add missing RBAC permission for operations related to `project.openshift.io`
+# This is required for the Ansible Task: `create project if projects are available`
+# TODO: Upstream PR to fix it: https://github.com/operator-framework/ansible-operator-plugins/pull/93
+kubectl patch clusterrole memcached-molecule-operator-manager-role --type='json' -p='[
+  {
+    "op": "add",
+    "path": "/rules/-",
+    "value": {
+      "apiGroups": ["project.openshift.io"],
+      "resources": ["projectrequests", "projects"],
+      "verbs": ["create", "delete", "get", "list", "patch", "update", "watch"]
+    }
+  }
+]'
 
-# switch to the "memcached-operator-system" namespace
-oc project memcached-operator-system
+# create clusterrolebinding for metrics
+kubectl create clusterrolebinding memcached-molecule-operator-metrics-reader-rolebinding --clusterrole=memcached-molecule-operator-metrics-reader --serviceaccount=memcached-molecule-operator-system:default
+
+# switch to the "memcached-molecule-operator-system" namespace
+oc project memcached-molecule-operator-system
 
 # Test the operator
 echo "running test_operator"
 test_operator
 
 # clean up the clusterrolebinding for metrics
-kubectl delete clusterrolebinding memcached-operator-metrics-reader-rolebinding
+kubectl delete clusterrolebinding memcached-molecule-operator-metrics-reader-rolebinding
 
 # remove operator
 echo "running make undeploy"
+# Hack: Adding `--ignore-not-found` to avoid errors when some kustomized resources are deleted with namespace deletion
+# TODO: Upstream PR to fix it: https://github.com/operator-framework/ansible-operator-plugins/pull/106
+sed -i '/kubectl delete/ s/$/ --ignore-not-found/' Makefile
 make undeploy
 
-# the memcached-operator pods remain after the deployment is gone; wait until the pods are removed
+# the memcached-molecule-operator pods remain after the deployment is gone; wait until the pods are removed
 echo "waiting for controller-manager pod to go away"
 if ! timeout 60s bash -c -- "until kubectl get pods -l control-plane=controller-manager |& grep \"No resources found\"; do sleep 2; done";
 then
-    echo FAIL: memcached-operator Deployment did not get garbage collected
+    echo FAIL: memcached-molecule-operator Deployment did not get garbage collected
     kubectl describe pods
-    kubectl logs deployment/memcached-operator-controller-manager -c manager
+    kubectl logs deployment/memcached-molecule-operator-controller-manager -c manager
     exit 1
 fi
 
